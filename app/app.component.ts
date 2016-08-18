32dc4b49af6ba60aa24dea3246d5d7d78e473f24
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2016-08-18 17:26:02 +0300

    fix modal view

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {knownFolders, path, Folder, File} from "file-system";
+import {ModalDialogService, ModalDialogOptions, ModalDialogHost} from "nativescript-angular/modal-dialog";
+import {ModalDialogParams} from "nativescript-angular/modal-dialog";
 import {isAndroid, isIOS} from "platform";
 import {Page} from "ui/page";
-import {WebView} from "ui/web-view"
+import {WebView} from "ui/web-view";
+import {alert} from "ui/dialogs";
+import { GestureEventData } from "ui/gestures";
+
 
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
+    directives: [ModalDialogHost],
+    providers: [ModalDialogService],
 })
-export class AppComponent {
-    public loadfile="";
-    constructor(){}
+export class AppComponent implements OnInit {
+    public loadfile="https://partners.adobe.com/public/developer/en/xml/AdobeXMLFormsSamples.pdf";
+    private calls = 0;
+    constructor(private _modalService: ModalDialogService){}
 
     public loadPdf(args:string){
         switch (args) {
@@ -26,6 +34,9 @@ export class AppComponent {
         }
     }
 
+    ngOnInit() {
+        alert("--Double Tap - to open on fullscreen--   --Long Press - to close fullscreen--");
+    }
 
     public loadfromfile(){
         var documents = knownFolders.documents();
@@ -40,4 +51,42 @@ export class AppComponent {
     public submit(args:string){
         this.loadfile = args;
     }
+
+    public createModelView(src:string) {
+        console.log("create modal view");
+        console.log("source "+src )
+        var that = this;
+        var options: ModalDialogOptions = {context:src,fullscreen: false};
+
+        this._modalService.showModal(PDFFullScreenComponent, options)
+            .then((result: string) => {
+                console.log("result "+result);
+                this.calls=0;
+            })
+    }
+}
+
+
+@Component({
+    selector: 'modal-content',
+    template: `
+    <StackLayout>
+        <PDFView (longPress)="close()" [src]="source"></PDFView>
+    </StackLayout>
+    `
+})
+export class PDFFullScreenComponent implements OnInit{
+    public source="";
+    constructor(private params: ModalDialogParams, private page: Page) {
+       console.log("mv "+this.params.context);
+    }
+    ngOnInit()
+    {
+         
+        this.source = this.params.context;
+    }
+
+     public close() {
+             this.params.closeCallback("fullscreen closed");
+     }
 }
